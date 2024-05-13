318f5cc0b22f81201e8ab50e5b50db8032cbfadf
md-imtiaj <89583416+md-imtiaj@users.noreply.github.com>
2024-05-13 21:16:57 +0530

    u

--- hugo-admin-post-editor-server.bat
+++ hugo-admin-post-editor-server.bat
@@ -2,6 +2,6 @@ start php -S localhost:80
 start http://localhost/hugo-admin-post-editor.php
 
 start hugo server
-start http://localhost:1313/
+start http://localhost:1313/freeware
 
 start https://web.archive.org/web/20210119065229/https://www.freewarefiles.com/
